ad7a7a1044ad23b092bedc330b317671289a3349
haseeb hummo <haseebpersonalwork@gmail.com>
2024-09-04 12:00:00 +0530

    Add project image imports and create projectsData array

---
+++ src/data/projectsData.js
@@ -0,0 +1,92 @@
+import project1Image1 from "/src/assets/Sukoon's Projects/1/KS_PareshBhai_DB_CH01_S01.png";
+import project1Image2 from "/src/assets/Sukoon's Projects/1/KS_PareshBhai_DB_CH01_S02.png";
+import project1Image3 from "/src/assets/Sukoon's Projects/1/KS_PareshBhai_DB_CH00_S03.png";
+
+import project2Image1 from "/src/assets/Sukoon's Projects/2/KS_LR_S01_CH00.png";
+import project2Image2 from "/src/assets/Sukoon's Projects/2/KS_LR_S02_CH00.png";
+
+import project3Image1 from "/src/assets/Sukoon's Projects/3/KS_P2_Lving Room_CH03_SH01.png";
+import project3Image2 from "/src/assets/Sukoon's Projects/3/KS_P2_Lving Room_CH02_SH04.png";
+import project3Image3 from "/src/assets/Sukoon's Projects/3/KS_P2_Lving Room_CH03_SH04.png";
+import project3Image4 from "/src/assets/Sukoon's Projects/3/KS_P2_Lving Room_CH03_SH07.png";
+import project3Image5 from "/src/assets/Sukoon's Projects/3/KS_P2_Lving Room_CH04_SH02.png";
+import project3Image6 from "/src/assets/Sukoon's Projects/3/KS_P2_Lving Room_CH04_SH04.png";
+import project3Image7 from "/src/assets/Sukoon's Projects/3/KS_P2_Lving Room_CH04_SH06.png";
+import project3Image8 from "/src/assets/Sukoon's Projects/3/KS_P2_Lving Room_CH04_SH07.png";
+
+import project4Image1 from "/src/assets/Sukoon's Projects/4/00008.png";
+import project4Image2 from "/src/assets/Sukoon's Projects/4/00009.png";
+
+import project5Image1 from "/src/assets/Sukoon's Projects/5/KS_DP_Kitchen02_SH01_FL.png";
+import project5Image2 from "/src/assets/Sukoon's Projects/5/KS_DP_Kitchen02_SH02_FL.png";
+import project5Image3 from "/src/assets/Sukoon's Projects/5/KS_DP_Kitchen02_SH03_FL.png";
+
+import project6Image1 from "/src/assets/Sukoon's Projects/6/KS_DP_Bedroom_SH01_CH00.png";
+import project6Image2 from "/src/assets/Sukoon's Projects/6/KS_DP_Bedroom_SH02_CH00.png";
+import project6Image3 from "/src/assets/Sukoon's Projects/6/KS_DP_Bedroom_SH03_CH00.png";
+import project6Image4 from "/src/assets/Sukoon's Projects/6/KS_DP_Bedroom_SH04_CH00.png";
+import project6Image5 from "/src/assets/Sukoon's Projects/6/KS_DP_Bedroom_SH05_CH00.png";
+
+import project7Image1 from "/src/assets/Sukoon's Projects/7/KS_PR_CH06_S01.png";
+import project7Image2 from "/src/assets/Sukoon's Projects/7/KS_PR_CH07_S02.png";
+import project7Image3 from "/src/assets/Sukoon's Projects/7/KS_PR_CH07_S03.png";
+
+import project8Image1 from "/src/assets/Sukoon's Projects/8/KS_GP_SH01_CH00.png";
+import project8Image2 from "/src/assets/Sukoon's Projects/8/KS_GP_SH02_CH00.png";
+import project8Image3 from "/src/assets/Sukoon's Projects/8/KS_PareshBhaig_Kitchen_SH01_CH00.png";
+import project8Image4 from "/src/assets/Sukoon's Projects/8/KS_PareshBhaig_Kitchen_SH02_CH00.png";
+import project8Image5 from "/src/assets/Sukoon's Projects/8/KS_PareshBhaig_Living_Room_SH01_CH01.png";
+import project8Image6 from "/src/assets/Sukoon's Projects/8/KS_PareshBhaig_Living_Room_SH03_CH01.png";
+
+const projectsData = [
+  {
+    id: 1,
+    name: "Bedroom | BedView",
+    images: [project1Image1, project1Image2, project1Image3],
+    thumbnail: project1Image1
+  },
+  {
+    id: 2,
+    name: "Project 2",
+    images: [project2Image1, project2Image2],
+    thumbnail: project2Image1
+  },
+  {
+    id: 3,
+    name: "Project 3",
+    images: [project3Image1, project3Image2, project3Image3, project3Image4, project3Image5, project3Image6, project3Image7, project3Image8],
+    thumbnail: project3Image1
+  },
+  {
+    id: 4,
+    name: "Project 4",
+    images: [project4Image1, project4Image2],
+    thumbnail: project4Image1
+  },
+  {
+    id: 5,
+    name: "Project 5",
+    images: [project5Image1, project5Image2, project5Image3],
+    thumbnail: project5Image1
+  },
+  {
+    id: 6,
+    name: "Project 6",
+    images: [project6Image1, project6Image2, project6Image3, project6Image4, project6Image5],
+    thumbnail: project6Image1
+  },
+  {
+    id: 7,
+    name: "Project 7",
+    images: [project7Image1, project7Image2, project7Image3],
+    thumbnail: project7Image1
+  },
+  {
+    id: 8,
+    name: "Project 8",
+    images: [project8Image1, project8Image2, project8Image3, project8Image4, project8Image5, project8Image6],
+    thumbnail: project8Image1
+  }
+];
+
+export default projectsData;
